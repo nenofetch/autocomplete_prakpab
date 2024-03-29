cab67620cd966e42d5f21cdbd8426e48a33f31dd
Neno Arisma <nenoarisma@gmail.com>
2024-03-30 00:18:39 +0700

    chore(main): update layout and main activity changing value from stringDataFak into dataProgramStudi for displaying on list view and autocomplete adding few activity (restart,resume and pause) changing color while the app going paused into green (check at line 70 on MainActivity.java)

--- app/src/main/java/com/nenofetch/autocompletepab/MainActivity.java
+++ app/src/main/java/com/nenofetch/autocompletepab/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nenofetch.autocompletepab;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -20,25 +22,59 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
 
     AutoCompleteTextView ac;
     public String[] stringdatafak = {"Fakultas Ekonomi", "Fakultas Kehutanan", "Fakultas Ilmu Komputer", "Fakultas Hukum", "Fakultas Keguruan dan Ilmu Pendidikan"};
-            EditText lv; @Override public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); setContentView(R.layout.activity_main);
+    public String[] dataProgramStudi = {"Teknik Informatika", "Sistem Informasi", "Desain Komunikasi Visual", "Manajemen Informatika D3", "Teknik Sipil"};
+    EditText lv;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         ac = (AutoCompleteTextView) findViewById(R.id.dataautocomplete);
         ac.addTextChangedListener(this);
         ac.setAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, stringdatafak));
+                android.R.layout.simple_dropdown_item_1line, dataProgramStudi));
     }
-    public void pilihfakultas (View v) { lv = (EditText)
-            findViewById(R.id.datalistview);
+
+    public void pilihfakultas(View v) {
+        lv = (EditText)
+                findViewById(R.id.datalistview);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Data Fakultas Universitas Kuningan");
-        builder.setItems(stringdatafak, new DialogInterface.OnClickListener() {
+        builder.setTitle("Data Program Studi Fakultas Ilmu Komputer UNIKU");
+        builder.setItems(dataProgramStudi, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int item) {
-                lv.setText(stringdatafak[item]);
+                lv.setText(dataProgramStudi[item]);
                 dialog.dismiss();
             }
         }).show();
     }
-    public void onTextChanged(CharSequence s, int start, int before, int count) { }
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-    public void afterTextChanged(Editable s) { }
+
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    public void afterTextChanged(Editable s) {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Resume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Pause", Toast.LENGTH_LONG).show();
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(Color.GREEN);
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "OK", Toast.LENGTH_LONG).show();
+    }
 }
